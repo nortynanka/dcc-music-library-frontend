ec8912651a8b7d4a891c9364ff24fc3ef92ccb15
Kayl Guise <kaylee.guise@gmail.com>
2022-04-19 15:51:56 -0500

    Songs and song information currently log to console

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import {useEffect, useState} from "react";
+
+
+import axios from "axios";
 
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    makeGetRequest();
+  },
+  [])
+ 
+  async function makeGetRequest(){
+    let response = await axios.get("http://www.devcodecampmusiclibrary.com/api/music");
+    console.log("response", response.data);
+  }
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
